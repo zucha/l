dc047181c91a0153fc143bd127d5d440b12c6935
Uldis Kirsis <uldis@sit.lv>
2020-10-23 12:50:43 +0300

    Item adapter with context

--- src/main/java/lv/sit/todo/ItemAdapter.java
+++ src/main/java/lv/sit/todo/ItemAdapter.java
@@ -1,22 +1,48 @@
 package lv.sit.todo;
 
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+import lv.sit.todo.db.Database;
+import lv.sit.todo.db.ItemDao;
 
 /**
  * @link https://www.geeksforgeeks.org/generics-in-java/
  * @link https://github.com/android/views-widgets-samples/blob/master/RecyclerView/Application/src/main/java/com/example/android/recyclerview/CustomAdapter.java
+ *
+ * Simple example:
+ * @link https://stackoverflow.com/questions/40584424/simple-android-recyclerview-example
  */
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    /**
+     *
+     */
+    private Context context;
 
+    public ItemAdapter (Context context)
+    {
+        Log.d(MainActivity.LOG_TAG, "Item adapter contructor");
+        this.context = context;
+
+
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        // this.context = MainActivity.context;
+
+        Log.d(MainActivity.LOG_TAG, "Create viwv holder");
+        Log.d(MainActivity.LOG_TAG, this.context.getClass().toString());
 
         // Create a new view.
         View v = LayoutInflater.from(parent.getContext())
@@ -25,20 +51,41 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
         return new ViewHolder(v);
     }
 
+    /**
+     * binds the data to the TextView in each row
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        // String animal = mData.get(position);
+        holder.rowTexView.setText("Test text");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        Database db = Room.inMemoryDatabaseBuilder(MainActivity.context, Database.class).build();
+        ItemDao itemDao = db.getItemDao();
+
+        return itemDao.getCount();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder
+    /**
+     *
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder
     {
+        /**
+         * bind row layout text view
+         */
+        public TextView rowTexView;
+
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+
+            rowTexView = (TextView) itemView.findViewById(R.id.rowTextViewId);
         }
     }
 }
--- src/main/java/lv/sit/todo/MainActivity.java
+++ src/main/java/lv/sit/todo/MainActivity.java
@@ -1,12 +1,15 @@
 package lv.sit.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import lv.sit.todo.db.Database;
@@ -15,17 +18,39 @@ import lv.sit.todo.db.ItemDao;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Tag for loging
+     */
+    public final static String LOG_TAG = "todoapp";
+
+    /**
+     * public access to application context
+     * @see android.app.Application
+     */
+    public static Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this.getApplicationContext();
+
+        Log.d(LOG_TAG, context.getClass().toString());
 
         insertRandomItem();
+
 
         /*TextView view = findViewById(R.id.textView2);
         view.setText("000");*/
         //
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.mainRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
+        ItemAdapter adapter = new ItemAdapter(context);
+        // adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     /**
